fdafae474814ca06e10b08daf065069b74b537b0
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-04-05 14:59:37 +0100

    adds a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-td3cun==mks339ta!r2u%gw=$1^u=8$5#b5#3bah8r822gw&-f'
+#SECRET_KEY = 'django-insecure-td3cun==mks339ta!r2u%gw=$1^u=8$5#b5#3bah8r822gw&-f'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,6 +90,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_H8eMNcSE2bqg@ep-black-frog-a21mffr1.eu-central-1.aws.neon.tech/pupil_blast_moist_424218")
+os.environ.setdefault("SECRET_KEY", "bH7o8Bko")
