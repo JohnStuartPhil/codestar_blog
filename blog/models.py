4bf5c39111711e20ca0cd9d718ba4dbd396a2c71
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-04-11 22:03:49 +0100

    model back to CommenT

--- blog/models.py
+++ blog/models.py
@@ -22,11 +22,11 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
-class Commentt(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="commentts")
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commentter")
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -31,11 +31,13 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     
+    
+
+    
     return render(
         request,
         "blog/post_detail.html",
         {
             "post": post
-            
         },
     )
